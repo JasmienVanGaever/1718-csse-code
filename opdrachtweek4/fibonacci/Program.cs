00769d2b7f14c95a65905306d6a243e11524996c
JasmienVanGaever <jasmvang@student.arteveldehs.be>
2017-10-23 23:08:33 +0200

    fibonacci

---
+++ opdrachtweek4/fibonacci/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uitvoerenFibonacci();
+        }
+
+            private static void uitvoerenFibonacci() {
+            //fibonacci oefening 01
+            int count = 0;
+            int a = 0;
+            int b = 1;
+
+            Console.Write(a + ", ");
+            Console.Write(b + ", ");
+
+            while(count <= 50) {
+
+                int c = a + b;
+                Console.Write(c + ", ");
+
+                a = b;
+                b = c;
+
+                count = count + 1;
+        
+            }
+        }
+    }
+}
